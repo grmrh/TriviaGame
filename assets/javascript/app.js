b20153577087e3fea455c78b1a76492628a02d82
kyhakw <kyhakw@hotmail.com>
2018-04-14 09:14:51 -0500

    complete, working version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,78 +1,217 @@
-/**
- * remaining time counter for each question. The time span for ecah question is 5 sec.
- */
-const qaList = [
-    {Q1: 'Q1',
-     A1: 'A1'}, 
-    {Q2: 'Q2', 
-     A2: 'A2'}, 
-    {Q3: 'Q3',
-     A3: 'A3'}
-]
 
-const timeSpanForEachQuestion = 5000;
-const tickerInterval = 1000;
-var ticker = timeSpanForEachQuestion;
-var questionNumber = 0;
-var askQuestion; 
-var askNextQuestion;
-var timer;
+function getTriviaQuestions() {
+    var option={
+        url: 'https://opentdb.com/api.php?amount=10&category=22&type=multiple', 
+        method: 'GET'
+    }
+    var list = [];
+    $.ajax(option)
+        .then(function(response) {
+            console.log(response);
+            if (response.results) {
+               trivia.triviaList = [];
+               //console.log(trivia.triviaList)
+               response.results.forEach(item =>list.push({
+                   Q: item.question,                   
+                   C: (function() {
+                        var index = Math.floor(Math.random()*(item.incorrect_answers.length));
+                        var collection = item.incorrect_answers;    
+                        collection.splice(index, 0, item.correct_answer);
+                        return collection;
+                   })(), 
+                   A: item.correct_answer,
+                   N: index-1
+                 }));
+            }
+            console.log("list" + list);
+     });
+};
 
-function remainingTimeCounter() {
-    ticker = ticker - tickerInterval;
-    displayRemainingTime(ticker);
+function trivia() {
+    var self = this;
+   function randomQA () {
+        var index = Math.floor(Math.random()*(self.triviaList.length));
+        console.log(index);
+        console.log(self.triviaList[index]);
+        self.triviaQA.Q = self.triviaList[index].Q;
+        self.triviaQA.A = self.triviaList[index].A;
+        self.triviaQA.C = self.triviaList[index].C.slice(0);
+        self.triviaQA.N = index;
+    }
+    this.triviaList = [
+        {Q: 'What is the official language of Greenland?',
+        C: ['English','Hindi', 'Greenlandic', 'Portuguese' ],
+        A: 'Greenlandic',
+        N: 2}, 
+        {Q: 'If you were in the city of Turin, what country would you be in?', 
+        C: ['USA', 'Russia', 'Scotland', 'Italy'], 
+        A: 'Italy',
+        N: 3}, 
+        {Q: "What is the capital city of Canada's Yukon territory?",
+        C: ['Winnepeg', 'Whitehorse','District of British Columbia', 'Edmonton' ], 
+        A: 'Whitehorse', 
+        N: 1},
+        {Q: 'Macau is an autonomous territory belonging to which country?',
+        C: ['China', 'USA', 'Japan', 'Cambodia'],
+        A: 'China', 
+        N: 0},
+        {Q: 'What are the colors that appear on the flag of France?',
+        C: ['Blue, white & red', 'Black, blue & white', 'Green, blue & whilte', 'Red, blue and white'],
+        A: 'Blue, white & red', 
+        N: 0 }, 
+        {Q: 'Which sea separates the East African coast and the Saudi Arabian peninsula?',
+        C: ['Yellow sea', 'Black sea', 'Blue sea', 'Red sea'],
+        A: 'Red sea', 
+        N: 3}];
+    this.triviaQA = {Q: "",
+                     C: [],
+                     A: "",
+                     N: 0};
+
+    //getTriviaQuestions();
+    //randomQA();
 }
 
-function displayRemainingTime(t) {
-    console.log("remaining time: " + t);
-    $(".remaining-time").text(t);
+const numberOfQ = 6;
+const qTimer = 5000;
+const tTimer = 1000;
+const tSpan = qTimer/tTimer;
+var qCounter = 0;
+var tickerCounter = tSpan;
+var correctCount=0; 
+var notCorrectCount=0;
+var notAnsweredCount=0;
+var timeoutId;
+var tickerId;
+
+tickerId = setInterval(tickerTimer, tTimer);
+
+function displayQA(qa) {
+    // display question
+    $(".card-body.answer").empty();
+    if (typeof qa != "undefined") {
+        $(".card-header").html("<Strong>Question " + qCounter+ "</strong>");
+        $(".card-title").text(qa.Q);
+        $("#optA0").next().text(qa.C[0]);
+        $("#optA1").next().text(qa.C[1]);
+        $("#optA2").next().text(qa.C[2]);
+        $("#optA3").next().text(qa.C[3]);
+        // start ticking
+        $(".card-body.qa").show();
+        //tickerId = setInterval(tickerTimer, tTimer);
+    }
+    else {
+        clearTimeout(timeoutId);
+        clearInterval(tickerId); 
+    }
 }
 
-function displayTriviaStat() {
-    console.log("trivia stat");
+function emptyQA() {
+    $(".card-title").empty();
+    $("#optA0").next().empty();
+    $("#optA1").next().empty();
+    $("#optA2").next().empty();
+    $("#optA3").next().empty();
+    $("input[type=radio]").attr("checked", false);
+    $("input:radio").prop("checked", false);
+    $("input:checked").empty();
 }
 
-function displayQuestion(qNumber) {
-    console.log("question number: " + qNumber);
+function CheckAnswer(qa, answerChecked) {
+
+    console.log("qa.N:" + qa.N);
+
+    emptyQA();
+    $(".card-body.qa").hide();
+
+    $(".card-body.answer").empty();
+    if (answerChecked == qa.N) {
+        correctCount++;
+        $(".card-body.answer").html("<h1><strong>You got that right!</strong></h1>");
+    }
+    else if (answerChecked != qa.N){
+        notCorrectCount++;
+        $(".card-body.answer").html("<h1><strong>Well... Next time...</strong></h1>" + 
+                                "<h1><strong>Correct Answer is " + qa.A + ".</strong></h1>");
+    }
+    else {
+        notAnsweredCount++;
+        $(".card-body.answer").html("<h1><strong>Timed out...</strong></h1>");
+    }
+    $(".card-body.answer").show();
+
+    // console.log(correctCount);
+    // console.log(notCorrectCount);
+    // console.log(notAnsweredCount);
+    if (numberOfQ == qCounter) {
+
+        clearTimeout(timeoutId);
+        clearInterval(tickerId); 
+
+        notAnsweredCount = numberOfQ - correctCount - notCorrectCount;
+        $(".card-body.answer").empty();
+        $(".card-body.answer").html("<h1><strong>Correct answers: " + correctCount + "</strong></h1>" + 
+                                    "<h1><strong>Wrong answers: " + notCorrectCount + "</strong></h1>" + 
+                                    "<h1><strong>Not answered: " + notAnsweredCount + "</strong></h1>");
+        $(".card-body.answer").show().focus();
+    }
 }
 
-function displayAnswerChoices(qNumber) {
-    console.log("answer number: " + qNumber);
-}
+$("input").on("click", function() {
 
-function nextQuestion() {
+    if (qCounter <= numberOfQ) {
+        answerChecked = $("input:checked").val();
+        console.log("answer checked " + answerChecked);
+
+        // clear ticking
+        //clearInterval(tickerId); 
+
+        // display answer
+        CheckAnswer(trivia.triviaList[qCounter-1], answerChecked);
+    }   
+});
+
+
+function tickerTimer() {
+    var d = new Date();
+    var t = d.toLocaleTimeString();
+    //document.getElementById(".remaining-time").innerHTML = t;
+    tickerCounter--;
+    $(".time-left").text(tickerCounter);
+
+    if (tickerCounter <= 0) {
+       console.log("Timed out");
+       notAnsweredCount++;
+       if (qCounter <= numberOfQ){
+        tickerCounter = tSpan;
+       }
+    }
+    //console.log("log k inside tickerTimer() " + tickerCounter);
+}
     
-    ticker = timeSpanForEachQuestion;
+function nextQ() {
+    qCounter++;
+    tickerCounter = tSpan;
 
-    questionNumber++;
-    console.log(questionNumber);
-    displayQuestion(questionNumber)
-    displayAnswerChoices(questionNumber);
-
-    if (questionNumber >= qaList.length) {
-        clearInterval(timer);
-        clearTimeout(askNextQuestion);  
-        clearInterval(askQuestion);
-        displayTriviaStat();     
+    if (qCounter > numberOfQ){       
+        clearTimeout(timeoutId);
+        clearInterval(tickerId); 
     }
 
-    askNextQuestion = setTimeout(function() {
-        
-        // display ticker
-        ticker = timeSpanForEachQuestion;
-        timer = setInterval(remainingTimeCounter, tickerInterval);
-    }, timeSpanForEachQuestion);
+    displayQA(trivia.triviaList[qCounter-1]);
 
-    
-    
+    // document.getElementById("demo2").innerHTML = t;
+    if (qCounter <= numberOfQ) {
+        //recursive call for accuracy
+        timeoutId = setTimeout(nextQ, qTimer);
+    }
+    else {
+        clearTimeout(timeoutId);
+        clearInterval(tickerId); 
+    }
 }
-
-
-$(document).ready(function() {
-
-    displayRemainingTime(timeSpanForEachQuestion);
-    // reset the remainingTimeCounter
     
-    //askQuestion = setTimeout(nextQuestion, timeSpanForEachQuestion* qaList.length);
-    askQuestion = setInterval(nextQuestion, timeSpanForEachQuestion);
-});
+var trivia = new trivia();
+console.log("triviaQA: " + trivia.triviaList);
+
+setTimeout(nextQ, 0);
